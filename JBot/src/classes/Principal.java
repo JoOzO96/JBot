8af5f79c1b7f389e44ab1006bc3bff4d9fed7c7e
Jose <Jose@Jose-PC>
2018-05-27 19:33:36 -0300

    ..

--- JBot/src/classes/Principal.java
+++ JBot/src/classes/Principal.java
@@ -7,8 +7,10 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.net.URL;
+import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -16,10 +18,12 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.plaf.SliderUI;
 
 import funcoes.Uteis;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.event.InputEvent;
 
 public class Principal {
 	private static Robot robot;
@@ -97,6 +101,7 @@ public class Principal {
 					public void run() {
 						// TODO Auto-generated method stub
 						clicanatela(robot, tipotreino, tipominerio, totalMinerio);
+//						Login(robot);
 					}
 				};
 				thread.start();
@@ -160,14 +165,16 @@ public class Principal {
 
 	}
 
-	public void clicanatela(Robot robot, String tipotreino,String tipominerio, Long totalMinerio) {
+	public void clicanatela(Robot robot, String tipotreino, String tipominerio, Long totalMinerio) {
 		boolean segue = true;
 		Long total = 0L;
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Date dataInicio = new Date();
 		int widthTela = (int) toolkit.getScreenSize().getWidth();
 		int heightTela = (int) toolkit.getScreenSize().getHeight();
 
 		while (segue) {
+
 			int y = 0;
 			int x = 0;
 			// SetDefault(robot, tipominerio);
@@ -175,7 +182,7 @@ public class Principal {
 			if (widthTela == 1336 && heightTela == 768) {
 				x = ThreadLocalRandom.current().nextInt(143, 173);
 				y = ThreadLocalRandom.current().nextInt(241, 285);
-			}else if (widthTela == 1920 && heightTela == 1080) {
+			} else if (widthTela == 1920 && heightTela == 1080) {
 				x = ThreadLocalRandom.current().nextInt(378, 419);
 				y = ThreadLocalRandom.current().nextInt(400, 445);
 			}
@@ -190,29 +197,79 @@ public class Principal {
 			if (widthTela == 1336 && heightTela == 768) {
 				x = ThreadLocalRandom.current().nextInt(1283, 1319);
 				y = ThreadLocalRandom.current().nextInt(362, 433);
-			}else if (widthTela == 1920 && heightTela == 1080) {
+			} else if (widthTela == 1920 && heightTela == 1080) {
 				x = ThreadLocalRandom.current().nextInt(1558, 1745);
 				y = ThreadLocalRandom.current().nextInt(528, 607);
 			}
 			uteis.click(robot, x, y);
 			robot.delay(6000);
 			robot.keyPress(KeyEvent.VK_SPACE);
-			
+
 			if (tipominerio.equals("Bronze")) {
 				robot.delay(33800);
 				total = total + 14;
 			} else if (tipominerio.equals("Mithril")) {
 				robot.delay(13800);
 				total = total + 5;
-			}else if (tipominerio.equals("Ferro")) {
-				robot.delay(21900);
+			} else if (tipominerio.equals("Aço")) {
+				robot.delay(21500);
 				total = total + 9;
 			}
 
 			if (total >= totalMinerio) {
 				segue = false;
 			}
+			long tempo = ThreadLocalRandom.current().nextInt(1300, 3600);
+			if ((System.currentTimeMillis() - dataInicio.getTime()) >= tempo) {
+				long tempoPausa = ThreadLocalRandom.current().nextInt(900, 1200);
+				
+				
+				try {
+					Thread.sleep(tempoPausa);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				Login(robot);
+				
+			}
 		}
+	}
+
+	private void Login(Robot robot2) {
+		String senha = "jose2alcides";
+		char[] charSenha = senha.toCharArray();
+		robot.mouseMove(895, 496);
+		robot.delay(15);
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.delay(200);
+		robot.mouseRelease(MouseEvent.BUTTON1_MASK);
+		for (int i = 0; i < senha.length(); i++) {
+			robot.delay(30);
+			robot2.keyPress(KeyEvent.getExtendedKeyCodeForChar(charSenha[i]));
+			robot.delay(30);
+			robot2.keyRelease(KeyEvent.getExtendedKeyCodeForChar(charSenha[i]));
+			robot.delay(30);
+		}
+		robot2.keyPress(KeyEvent.VK_ENTER);
+		robot2.delay(30);
+		robot2.keyRelease(KeyEvent.VK_ENTER);
+		robot.delay(4000);
+		robot.mouseMove(ThreadLocalRandom.current().nextInt(877, 1038), ThreadLocalRandom.current().nextInt(545, 566));
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.delay(200);
+		robot.mouseRelease(MouseEvent.BUTTON1_MASK);
+		robot.delay(10000);
+		robot.mouseMove(ThreadLocalRandom.current().nextInt(1727, 1743), ThreadLocalRandom.current().nextInt(69, 87));
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.delay(200);
+		robot.mouseRelease(MouseEvent.BUTTON1_MASK);
+		robot.keyPress(KeyEvent.VK_UP);
+		robot.delay(500);
+		robot.keyRelease(KeyEvent.VK_UP);
+		
+		
 	}
 
 }
