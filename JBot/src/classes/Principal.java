f8de3b296227c974d4a0cead63e2697240ccee82
Jose <141865@upf.br>
2018-05-29 17:47:58 -0300

    ..

--- JBot/src/classes/Principal.java
+++ JBot/src/classes/Principal.java
@@ -34,9 +34,11 @@ public class Principal {
 	private static ClicanaTela tela;
 	private static JComboBox cb_tipotreino;
 	private static JComboBox cb_tipominerio;
+	private static JComboBox comboBox;
 	private String tipominerio;
 	private Long totalMinerio;
 	private String tipotreino;
+	private Long preset;
 	public static JTextField tx_total;
 	public static JTextField tx_minerios;
 	public static JTextField tx_tempo;
@@ -67,12 +69,14 @@ public class Principal {
 					robot = new Robot();
 					tela = new ClicanaTela();
 					cb_tipominerio.setEnabled(false);
-					cb_tipotreino.addItem("Metalurgia");
-					cb_tipominerio.addItem("Bronze");
-					cb_tipominerio.addItem("Ferro");
-					cb_tipominerio.addItem("Aço");
-					cb_tipominerio.addItem("Ouro");
-					cb_tipominerio.addItem("Mithril");
+//					cb_tipotreino.addItem("Metalurgia");
+//					cb_tipominerio.addItem("Bronze");
+//					cb_tipominerio.addItem("Ferro");
+//					cb_tipominerio.addItem("Aço");
+//					cb_tipominerio.addItem("Ouro");
+//					cb_tipominerio.addItem("Mithril");
+					comboBox.addItem("1");
+					comboBox.addItem("2");
 					tx_total.setText(String.valueOf(0));
 
 					// uteis.SetDefault();
@@ -103,7 +107,7 @@ public class Principal {
 					public void run() {
 						// TODO Auto-generated method stub
 						clicanatela(robot, tipotreino, tipominerio, totalMinerio);
-//						Login(robot);
+						// Login(robot);
 					}
 				};
 				thread.start();
@@ -127,6 +131,7 @@ public class Principal {
 		frame.getContentPane().add(btnNewButton_1);
 
 		cb_tipotreino = new JComboBox();
+		cb_tipotreino.setModel(new DefaultComboBoxModel(new String[] {"Metalurgia"}));
 		cb_tipotreino.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				tipotreino = cb_tipotreino.getSelectedItem().toString();
@@ -139,11 +144,13 @@ public class Principal {
 		frame.getContentPane().add(cb_tipotreino);
 
 		cb_tipominerio = new JComboBox();
-		cb_tipominerio.setModel(new DefaultComboBoxModel(new String[] {"Bronze", "Ferro", "A\u00E7o", "Ouro", "Mithril"}));
+		cb_tipominerio
+				.setModel(new DefaultComboBoxModel(new String[] {"Bronze", "Ferro", "A\u00E7o", "Ouro", "Mithril"}));
 		cb_tipominerio.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				tipominerio = cb_tipominerio.getSelectedItem().toString();
 			}
+
 		});
 		cb_tipominerio.setBounds(44, 118, 197, 20);
 		frame.getContentPane().add(cb_tipominerio);
@@ -163,8 +170,17 @@ public class Principal {
 		tx_tempo = new JTextField();
 		tx_tempo.setText("0");
 		tx_tempo.setColumns(10);
-		tx_tempo.setBounds(119, 165, 63, 20);
+		tx_tempo.setBounds(120, 185, 63, 20);
 		frame.getContentPane().add(tx_tempo);
+
+		comboBox = new JComboBox();
+		comboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				preset = Long.parseLong(comboBox.getSelectedItem().toString());
+			}
+		});
+		comboBox.setBounds(117, 149, 66, 20);
+		frame.getContentPane().add(comboBox);
 
 	}
 
@@ -182,9 +198,9 @@ public class Principal {
 			int x = 0;
 			// SetDefault(robot, tipominerio);
 			Random random = new Random();
-			if (widthTela == 1336 && heightTela == 768) {
-				x = ThreadLocalRandom.current().nextInt(143, 173);
-				y = ThreadLocalRandom.current().nextInt(241, 285);
+			if (widthTela == 1366 && heightTela == 768) {
+				x = ThreadLocalRandom.current().nextInt(112, 140);
+				y = ThreadLocalRandom.current().nextInt(235, 287);
 			} else if (widthTela == 1920 && heightTela == 1080) {
 				x = ThreadLocalRandom.current().nextInt(378, 419);
 				y = ThreadLocalRandom.current().nextInt(400, 445);
@@ -193,13 +209,21 @@ public class Principal {
 			uteis.click(robot, x, y);
 			// robot.delay(1000);
 			robot.delay(6200);
-			robot.keyPress(KeyEvent.VK_1);
-			robot.delay(100);
-			robot.keyRelease(KeyEvent.VK_1);
-			robot.delay(500);
-			if (widthTela == 1336 && heightTela == 768) {
-				x = ThreadLocalRandom.current().nextInt(1283, 1319);
-				y = ThreadLocalRandom.current().nextInt(362, 433);
+
+			if (preset == 1) {
+				robot.keyPress(KeyEvent.VK_1);
+				robot.delay(100);
+				robot.keyRelease(KeyEvent.VK_1);
+				robot.delay(500);
+			} else {
+				robot.keyPress(KeyEvent.VK_2);
+				robot.delay(100);
+				robot.keyRelease(KeyEvent.VK_2);
+				robot.delay(500);
+			}
+			if (widthTela == 1366 && heightTela == 768) {
+				x = ThreadLocalRandom.current().nextInt(1272, 1320);
+				y = ThreadLocalRandom.current().nextInt(379, 445);
 			} else if (widthTela == 1920 && heightTela == 1080) {
 				x = ThreadLocalRandom.current().nextInt(1558, 1745);
 				y = ThreadLocalRandom.current().nextInt(528, 607);
@@ -217,6 +241,14 @@ public class Principal {
 			} else if (tipominerio.equals("Aço")) {
 				robot.delay(22200);
 				total = total + 9;
+			} else if (tipominerio.equals("Ouro")) {
+				try {
+					Thread.sleep(68800);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				total = total + 28;
 			}
 
 			if (total >= totalMinerio) {
@@ -225,24 +257,31 @@ public class Principal {
 			long tempo = ThreadLocalRandom.current().nextInt(25000, 36000);
 			if ((System.currentTimeMillis() - dataInicio.getTime()) <= tempo) {
 				long tempoPausa = ThreadLocalRandom.current().nextInt(9000, 12000);
-				
+
 				try {
 					Thread.sleep(tempoPausa);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 				Login(robot);
-				
+
 			}
 		}
 	}
 
 	private void Login(Robot robot2) {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		int widthTela = (int) toolkit.getScreenSize().getWidth();
+		int heightTela = (int) toolkit.getScreenSize().getHeight();
 		String senha = "jose2alcides";
 		char[] charSenha = senha.toCharArray();
-		robot.mouseMove(895, 496);
+		if (widthTela == 1366 && heightTela == 768) {
+			robot.mouseMove(617, 387);
+		} else if (widthTela == 1920 && heightTela == 1080) {
+			robot.mouseMove(895, 496);
+		}
 		robot.delay(15);
 		robot.mousePress(InputEvent.BUTTON1_MASK);
 		robot.delay(200);
@@ -258,20 +297,28 @@ public class Principal {
 		robot2.delay(30);
 		robot2.keyRelease(KeyEvent.VK_ENTER);
 		robot.delay(4000);
-		robot.mouseMove(ThreadLocalRandom.current().nextInt(877, 1038), ThreadLocalRandom.current().nextInt(545, 566));
+		
+		if (widthTela == 1366 && heightTela == 768) {
+			robot.mouseMove(ThreadLocalRandom.current().nextInt(609, 757), ThreadLocalRandom.current().nextInt(543, 563));
+		} else if (widthTela == 1920 && heightTela == 1080) {
+			robot.mouseMove(ThreadLocalRandom.current().nextInt(877, 1038), ThreadLocalRandom.current().nextInt(545, 566));
+		}
+		
 		robot.mousePress(InputEvent.BUTTON1_MASK);
 		robot.delay(200);
 		robot.mouseRelease(MouseEvent.BUTTON1_MASK);
 		robot.delay(10000);
-		robot.mouseMove(ThreadLocalRandom.current().nextInt(1727, 1743), ThreadLocalRandom.current().nextInt(69, 87));
+		if (widthTela == 1366 && heightTela == 768) {
+			robot.mouseMove(ThreadLocalRandom.current().nextInt(1173, 1187), ThreadLocalRandom.current().nextInt(67, 85));
+		} else if (widthTela == 1920 && heightTela == 1080) {
+			robot.mouseMove(ThreadLocalRandom.current().nextInt(1727, 1743), ThreadLocalRandom.current().nextInt(69, 87));
+		}
 		robot.mousePress(InputEvent.BUTTON1_MASK);
 		robot.delay(200);
 		robot.mouseRelease(MouseEvent.BUTTON1_MASK);
 		robot.keyPress(KeyEvent.VK_UP);
 		robot.delay(1000);
 		robot.keyRelease(KeyEvent.VK_UP);
-		
-		
-	}
 
+	}
 }
